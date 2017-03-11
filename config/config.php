55555356aea556b7d286e40d59dc79b092dc55c7
shtoorman <bar2683@mail.ru>
2017-03-11 21:09:07 +0200

    'chenges'

--- config/config.php
+++ config/config.php
@@ -1,4 +1,22 @@
 <?php
 //Осовой конфигурационный файл приложения
 //Параметры азвания сайта или подключения к базе данных
-Config::set('$site_name','Your Site Name');
+Config::set('$site_name', 'Your Site Name');
+//Определения списка языков
+Config::set('languages', array('ru', 'en'));
+
+//Routes, Route name => method prefix
+//2 роута,дкфолт и админ
+Config::set('routes', array(
+    'default' => '',
+    'admin' => 'admin',
+));
+//если роут не зада явно будет использован дифолт
+Config::set('default_route', 'default');
+//язык по умолчанию ru.если не указан в url явно
+Config::set('default_language', 'ru');
+//контроллер по умолчанию pages
+Config::set('default_controller', 'pages');
+
+Config::set('default-action', 'index');
+
