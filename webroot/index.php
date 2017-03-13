d202a424195fef162362452210639b9e8668fb9c
shtoorman <bar2683@mail.ru>
2017-03-13 14:31:26 +0200

    add router constructor

--- webroot/index.php
+++ webroot/index.php
@@ -12,6 +12,16 @@ require_once (ROOT.DS.'lib'.DS.'init.php');
 
 $router = new Router($_SERVER['REQUEST_URI']);
 
+echo "<pre>";
+//PHP_EOL константк перевода строки
+print_r('Route: '.$router->getRoute().PHP_EOL);
+print_r('Language: '.$router->getLanguage().PHP_EOL);
+print_r('Controller: '.$router->getController().PHP_EOL);
+print_r('Action to be called: '.$router->getMethodPrefix().$router->getAction().PHP_EOL);
+echo "Params: ";
+print_r($router->getParams());
+
+
 
 //echo 'Root->'.ROOT;
 //Строка запроса что пришла в индекс
